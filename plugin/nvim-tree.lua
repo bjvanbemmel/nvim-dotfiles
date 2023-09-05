758045eaaa467d6cb915bdb04c16d94e40241ed1
Beau Jean van Bemmel <bj.vanbemmel@iso2handle.nl>
2023-09-05 12:11:09 +0200

    open nvim-tree on startup

--- plugin/nvim-tree.lua
+++ plugin/nvim-tree.lua
@@ -27,3 +27,16 @@ require("nvim-tree").setup({
         }
     }
 })
+
+local function open_nvim_tree(data)
+    -- buffer is a directory
+    local directory = vim.fn.isdirectory(data.file) == 1
+
+    if not directory then
+        return
+    end
+
+    require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
